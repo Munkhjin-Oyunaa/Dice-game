0643076ac9ff48dd7188d0186cf9525e004ebdce
munkhjin <munkhjin-oyunaa@gmail.com>
2026-02-03 23:12:02 +0800

    finished

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Togloomiin buh gazart ashigladah global huvisagchdiig end zarlay
 
+//togloom duussan esehiig shalgah huvisagch
+var isNewGame;
+
 // Ali toglogchiin eeljiig harah huvisagch
 var activePlayer;
 // Hoyr toglogchiin tsugluulsan onoonuud
@@ -13,6 +16,9 @@ var diceDom = document.querySelector(".dice");
 initGame();
 
 function initGame() {
+  // togloom ehellee gedeg tuluvt oruulna.
+  isNewGame = true;
+
   // Toglogchiin eeljiig hadgalah huvisagch, negdugeer toglogchiig 0, hoyrdugaar toglogchiig 1 gej temdegley.
   activePlayer = 0;
 
@@ -46,57 +52,68 @@ function initGame() {
   diceDom.style.display = "none";
 }
 // Shoog shideh event listener
-
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  // 1-6 dotorh sanamsargui neg too gargaj avna
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  if (isNewGame) {
+    // 1-6 dotorh sanamsargui neg too gargaj avna
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-  // shoonii zurgiig web deer gargaj irne.
-  diceDom.style.display = "block";
+    // shoonii zurgiig web deer gargaj irne.
+    diceDom.style.display = "block";
 
-  // Buusan sanamsargui toond hargalzah shoonii zurgiig web deer gargaj irne.
-  diceDom.src = "dice-" + diceNumber + ".png";
+    // Buusan sanamsargui toond hargalzah shoonii zurgiig web deer gargaj irne.
+    diceDom.src = "dice-" + diceNumber + ".png";
 
-  //Buusan too n 1 ees yalgaatai bol idevhtei toglogchiin eeljiin onoog uurchilnu
-  if (diceNumber !== 1) {
-    //1-ees yalgaatai too buulaa. Buusan toog toglchid nemj ugnuu
-    roundScore = roundScore + diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
+    //Buusan too n 1 ees yalgaatai bol idevhtei toglogchiin eeljiin onoog uurchilnu
+    if (diceNumber !== 1) {
+      //1-ees yalgaatai too buulaa. Buusan toog toglchid nemj ugnuu
+      roundScore = roundScore + diceNumber;
+      document.getElementById("current-" + activePlayer).textContent =
+        roundScore;
+    } else {
+      // 1 buusan tul toglogchiin eeljiig ene hesegt solij ugnu.
+
+      switchToNextPlayer();
+    }
   } else {
-    // 1 buusan tul toglogchiin eeljiig ene hesegt solij ugnu.
-
-    switchToNextPlayer();
+    alert("Game Over");
   }
 });
 
 // HOLD tovchnii event listener
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  // Ug toglogchiin tsugluulsan eeljnii onoog global onoon deer n nemj ugnu.
-  //   if (activePlayer === 0) {
-  //     scores[0] = scores[0] + roundScore;
-  //   } else {
-  //     scores[1] = scores[1] + roundScore;
-  //   }
+  if (isNewGame) {
+    // Ug toglogchiin tsugluulsan eeljnii onoog global onoon deer n nemj ugnu.
+    //   if (activePlayer === 0) {
+    //     scores[0] = scores[0] + roundScore;
+    //   } else {
+    //     scores[1] = scores[1] + roundScore;
+    //   }
 
-  scores[activePlayer] = scores[activePlayer] + roundScore;
+    scores[activePlayer] = scores[activePlayer] + roundScore;
 
-  // Delgets deer onoog n uurchilnu
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
+    // Delgets deer onoog n uurchilnu
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
 
-  // Ug toglogch hojson esehiig (onoo n 100-s ih eseh) shalgah
-  if (scores[activePlayer] >= 10) {
-    // Ylagch gesen textiig nerniih n orond gargana
-    document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
+    // Ug toglogch hojson esehiig (onoo n 100-s ih eseh) shalgah
+    if (scores[activePlayer] >= 10) {
+      //togloom duusgah
+      isNewGame = false;
+
+      // Ylagch gesen textiig nerniih n orond gargana
+      document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+    } else {
+      //toglogchiin eeljiig solino.
+      switchToNextPlayer();
+    }
   } else {
-    //toglogchiin eeljiig solino.
-    switchToNextPlayer();
+    alert("game over");
   }
 });
 
